158bcc424f82f18a08a6a3568b5d3d0dde0c1af5
Shtiroy Alexandr <alexandr.stiroi@gmail.com>
2021-06-11 12:57:18 +0300

    Added class employee repository

---
+++ src/main/java/org/shtiroy/factory/repository/EmployeeRepository.java
@@ -0,0 +1,25 @@
+package org.shtiroy.factory.repository;
+
+import org.shtiroy.factory.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findAllByWorks(Boolean works);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE working_data.t_employee SET first_name = ?1, last_name = ?2, " +
+                "middle_name = ?3, date_birth = ?4, works = ?5 WHERE id = ?6",
+            nativeQuery = true)
+    void updateEmployee(String firstName, String lastName, String middleName, Date date_birth, Long id);
+
+}
