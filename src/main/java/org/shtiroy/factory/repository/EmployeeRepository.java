09e4d2d97dcf4c421f23313dae40d984857b19f3
Shtiroy Alexandr <alexandr.stiroi@gmail.com>
2021-06-11 13:05:18 +0300

    Updated employee repository

--- src/main/java/org/shtiroy/factory/repository/EmployeeRepository.java
+++ src/main/java/org/shtiroy/factory/repository/EmployeeRepository.java
@@ -20,6 +20,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query(value = "UPDATE working_data.t_employee SET first_name = ?1, last_name = ?2, " +
                 "middle_name = ?3, date_birth = ?4, works = ?5 WHERE id = ?6",
             nativeQuery = true)
-    void updateEmployee(String firstName, String lastName, String middleName, Date date_birth, Long id);
+    void updateEmployee(String firstName, String lastName, String middleName, Date dateBirth, Boolean works, Long id);
 
 }
